e9f9facb8308beca7ece4806fb587f966f29a877
Chaitanya2801 <chaitanya.agarwal_iot18@gla.ac.in>
2020-03-31 16:56:39 +0530

    creating a public class insert to find the value of the node

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,21 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    static Queue<TreeNode> q;
+
+    public TreeNode insert(TreeNode root, int x) {
+        if (root == null)
+            return new TreeNode(x);
+        else if (x > root.getData())
+            root.setRight(insert(root.getRight(), x));
+        else
+            root.setLeft(insert(root.getLeft(), x));
+        return root;
+    }
+
 }
+
