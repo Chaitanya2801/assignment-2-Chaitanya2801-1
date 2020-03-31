8ee461f13dc3c8ac1881ac4d1405e21c6290f2b1
Chaitanya2801 <chaitanya.agarwal_iot18@gla.ac.in>
2020-03-31 17:04:12 +0530

    performing operations to extract the left child the whole binary tree

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -64,5 +64,102 @@ public class MyBinarySearchTree {
 
     }
 
+    public void countNodeWithOutLeftChild(TreeNode root) {
+
+        if (root == null)
+
+            return;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        queue.add(root);
+
+        int count = 0;
+
+        while (queue != null) {
+
+            TreeNode temp = queue.poll();
+
+            if (temp == null) {
+
+                break;
+
+            }
+            if (temp.getLeft() == null)
+
+                count++;
+
+            if (temp.getLeft() != null)
+
+                queue.add(temp.getLeft());
+
+            if (temp.getRight() != null)
+
+                queue.add(temp.getRight());
+
+        }
+
+        System.out.println("count of Nodes without left child: " + count);
+
+    }
+
+    public void printLeftTree(TreeNode root) {
+
+        if (root == null)
+
+            return;
+
+        q.add(root);
+
+        q.add(null);
+
+
+        while (q.size() > 0) {
+
+            TreeNode temp = q.peek();
+
+
+            if (temp != null) {
+
+                System.out.print(temp.getData() + " ");
+
+                while (q.peek() != null) {
+
+
+                    if (temp.getLeft() != null)
+
+                        q.add(temp.getLeft());
+
+                    if (temp.getRight() != null)
+
+                        q.add(temp.getRight());
+
+
+
+                    q.remove();
+
+
+                    temp = q.peek();
+
+                }
+
+                q.add(null);
+
+            }
+
+            q.remove();
+
+        }
+
+    }
+
+    public void leftView(TreeNode root) {
+
+        q = new LinkedList<TreeNode>();
+
+        printLeftTree(root);
+
+    }
+
 }
 
